00b53b0545c62d6f327d861f438b2f1385a5abd6
Brady <thevillemc@gmail.com>
2021-01-12 20:28:41 +0000

    set up basics for keyboard inputs

--- client.js
+++ client.js
@@ -18,6 +18,7 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: BAB");
+    // setInterval(() => conn.write("Move: up"), 500);
   });
 
   return conn;
--- play.js
+++ play.js
@@ -1,3 +1,28 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function (handleUserInput) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === `\u0003`) {
+    console.log(`Thanks for playing!`);
+    process.exit();
+  }
+};
+
+
+setupInput(handleUserInput);
