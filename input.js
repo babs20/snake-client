f52b0fa3554d12e18a85815983cc2b54f9bcf7f5
Brady <thevillemc@gmail.com>
2021-01-12 20:55:51 +0000

    added user input functionality to be able to move

--- input.js
+++ input.js
@@ -1,22 +1,40 @@
+let connection;
+
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
-    handleUserInput(key);
+    handleUserInput(key, connection);
   });
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function (key, conn) {
+  //stop game
   if (key === `\u0003`) {
     console.log(`Thanks for playing!`);
     process.exit();
+  }
+
+  //movement
+  if (key === 'w') {
+    conn.write('Move: up');
+  }
+  if (key === 'a') {
+    conn.write('Move: left');
+  }
+  if (key === 's') {
+    conn.write('Move: down');
+  }
+  if (key === 'd') {
+    conn.write('Move: right');
   }
 };
 
