2e8e119914d84aae0dd4bf5bb23123af760ca5b8
Brady <thevillemc@gmail.com>
2021-01-12 20:03:48 +0000

    added name to the snake on connection

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: BAB");
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
